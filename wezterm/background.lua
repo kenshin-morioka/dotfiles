2d08af4939039dc3f493c6a3b5420c7ad6c90bb5
kenshin-morioka <kenshin.ml012@gmail.com>
2025-12-30 21:21:43 +0900

    背景設定の修正と、その他修正

---
+++ wezterm/background.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+-- 背景画像のパスを設定
+local background_image = "/Users/moriokakenshin/src/github.com/kenshin-morioka/dotfiles/wezterm/nerve.jpg"
+
+return {
+    {
+        source = {
+            Gradient = {
+            colors = { "#08607dff", "#001522" },
+            orientation = {
+                Linear = { angle = -30.0 },
+            },
+            },
+        },
+        opacity = 0.75,
+        width = "100%",
+        height = "100%",
+    },
+    {
+        source = { File = background_image },
+        opacity = 0.12,
+        vertical_align = "Middle",
+        horizontal_align = "Right",
+        horizontal_offset = "200px",
+        repeat_x = "NoRepeat",
+        repeat_y = "NoRepeat",
+        -- width = "1431px",
+        -- height = "1900px"
+    },
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,16 +1,31 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.font_size = 12.0
+config.use_ime = true
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_frame = {
+  font = wezterm.font { family ='Roboto', weight = 'Bold' },
+  font_size = 11.0,
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+
+config.show_new_tab_button_in_tab_bar = false
+-- config.show_close_tab_button_in_tabs = true
+
 require 'format'
 require 'status'
 require 'event'
 require 'keybinds'
 
-local wezterm = require 'wezterm';
+
 local keybind = require 'keybinds'
-
-local config = {}
-
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local background = require 'background'
 
 -- keybinds
 -- デフォルトのkeybindを無効化
@@ -23,26 +38,13 @@ config.key_tables = keybind.key_tables
 -- Leaderキーの設定
 config.leader = { key = ",", mods = "CTRL", timeout_milliseconds = 2000 }
 
--- colors
-config.color_scheme = "nord"
-config.window_background_opacity = 0.93
-
--- font
--- config.font = wezterm.font("Firge35Nerd Console")
-config.font_size = 16.0
-config.window_frame = {
-  font = wezterm.font { family ='Roboto', weight = 'Bold' },
-  font_size = 11.0,
-}
-
 -- status
 config.status_update_interval = 1000
 
--- window decorations
-config.window_decorations = "RESIZE"
+-- background
+config.background = background
 
 -- mouse binds
 -- config.mouse_bindings = require('mousebinds').mouse_bindings
 
 return config
-
