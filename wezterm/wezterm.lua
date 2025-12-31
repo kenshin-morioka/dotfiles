09a24c2b2388aa6d790617fa00ddeada0bc8b0b1
kenshin-morioka <kenshin.ml012@gmail.com>
2025-12-31 20:17:32 +0900

    update wezter font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
-config.font_size = 12.0
+config.font_size = 15.0
 config.use_ime = true
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
