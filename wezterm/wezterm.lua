802eb2533566fb29da99cdee5215be8047d20714
kenshin-morioka <kenshin.ml012@gmail.com>
2025-12-31 11:02:09 +0900

    タブのUIを修正と、その他リファクタ

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,43 +6,72 @@ config.font_size = 12.0
 config.use_ime = true
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
+-- config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+-- config.show_close_tab_button_in_tabs = true
 
 config.window_frame = {
   font = wezterm.font { family ='Roboto', weight = 'Bold' },
-  font_size = 11.0,
+  font_size = 13.0,
   inactive_titlebar_bg = "none",
   active_titlebar_bg = "none",
 }
 
-config.show_new_tab_button_in_tab_bar = false
--- config.show_close_tab_button_in_tabs = true
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
+
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
+-- keybindの設定
+local keybind = require 'keybinds'
+config.disable_default_key_bindings = true
+config.keys = keybind.keys
+config.key_tables = keybind.key_tables
+
+-- background
+local background = require 'background'
+config.background = background
 
 require 'format'
 require 'status'
 require 'event'
-require 'keybinds'
 
-
-local keybind = require 'keybinds'
-local background = require 'background'
-
--- keybinds
--- デフォルトのkeybindを無効化
-config.disable_default_key_bindings = true
-
--- keybindの設定
-config.keys = keybind.keys
-config.key_tables = keybind.key_tables
 
 -- Leaderキーの設定
 config.leader = { key = ",", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- status
 config.status_update_interval = 1000
-
--- background
-config.background = background
 
 -- mouse binds
 -- config.mouse_bindings = require('mousebinds').mouse_bindings
