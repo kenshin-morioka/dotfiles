fabfa7b6a70fd9cc56e71741c5b521cd1c2576da
kenshin-morioka <kenshin.ml012@gmail.com>
2026-06-02 15:13:09 +0900

    feat: h/l キーにも長押し加速を適用
    accelerated-jk.nvim の acceleration_motions に h/l を追加し、
    横方向の移動も j/k と同じ加速カーブで動作するようにする。

--- nvim/lua/extensions/accelerated-jk.lua
+++ nvim/lua/extensions/accelerated-jk.lua
@@ -1,7 +1,7 @@
 require('accelerated-jk').setup {
   mode = 'time_driven',
   enable_deceleration = false,
-  acceleration_motions = {},
+  acceleration_motions = { 'h', 'l' },
   acceleration_limit = 150,
   acceleration_table = { 7, 12, 17, 21, 24, 26, 28, 30 },
   deceleration_table = { { 150, 9999 } },
